16013d4045ab9cfcf8b3d253174da2ae3c5deb59
Lei Song <lsong@clarku.edu>
2022-06-18 11:52:17 -0400

    Update code

--- scripts/upscaling.R
+++ scripts/upscaling.R
@@ -1,6 +1,4 @@
-library(here)
 library(terra)
-library(dplyr)
 library(APUpscale)
 
 data_path <- here('data')
@@ -11,42 +9,29 @@ fname <- file.path(data_path, 'landcover.tif')
 # scales: fine-scale, medium-scale, medium-scale, 
 # medium-scale, coarse-scale
 #################################################
-# Upscale to 1000m
-landscape <- rast(fname)
-landscape <- upscale(landscape, cellsize = 1000, verbose = TRUE)
-writeRaster(landscape, file.path(data_path, 'landcover_1000m.tif'),
-            overwrite = T,
-            wopt = list(datatype = 'INT1U',
-                        gdal=c("COMPRESS=LZW")))
-
-# Upscale to 500m
-landscape <- rast(fname)
-landscape <- upscale(landscape, cellsize = 500)
-writeRaster(landscape, file.path(data_path, 'landcover_500m.tif'),
-            overwrite = T,
-            wopt = list(datatype = 'INT1U',
-                        gdal=c("COMPRESS=LZW")))
-
-# Upscale to 250m
-landscape <- rast(fname)
-landscape <- upscale(landscape, cellsize = 250, verbose = TRUE)
-writeRaster(landscape, file.path(data_path, 'landcover_250m.tif'),
-            overwrite = T,
-            wopt = list(datatype = 'INT1U',
-                        gdal=c("COMPRESS=LZW")))
-
-# Upscale to 100m
-landscape <- rast(fname)
-landscape <- upscale(landscape, cellsize = 100, verbose = TRUE)
-writeRaster(landscape, file.path(data_path, 'landcover_100m.tif'),
-            overwrite = T,
-            wopt = list(datatype = 'INT1U',
-                        gdal=c("COMPRESS=LZW")))
-
-# Upscale to 30m
-landscape <- rast(fname)
-landscape <- upscale(landscape, cellsize = 30, verbose = TRUE)
-writeRaster(landscape, file.path(data_path, 'landcover_30m.tif'),
-            overwrite = T,
-            wopt = list(datatype = 'INT1U',
-                        gdal=c("COMPRESS=LZW")))
+upscale_lc <- function(fname, cellsize, verbose, dst_fname) {
+    landscape <- rast(fname)
+    # the factor of upscaling to e.g. 30m is too small. Almost all pixels would 
+    # have the majority class, so here we just use majority method for upscaling 
+    # for speed.
+    if (cellsize < 100) {
+        output <- rast(ext(landscape), crs = crs(landscape),
+                       resolution = cellsize,
+                       vals = NA)
+        landscape <- resample(landscape, output, method = "mode")
+        writeRaster(landscape, dst_fname,
+                    overwrite = T,
+                    wopt = list(datatype = 'INT1U',
+                                gdal=c("COMPRESS=LZW")))
+    # Use area preserving method for 
+        # coarse resolution of 100m, 250m, 500m, 1000m
+    } else {
+        landscape <- upscale(landscape, 
+                             cellsize = cellsize, 
+                             verbose = verbose)
+        writeRaster(landscape, file.path(dst_fname),
+                    overwrite = T,
+                    wopt = list(datatype = 'INT1U',
+                                gdal=c("COMPRESS=LZW")))
+    }
+}
